b3da7259096137505383035f1cfa93eaf469b7df
spk <ashutoshkarande5@gmail.com>
2020-12-23 17:32:12 +0530

    reuse

--- components/src/index.js
+++ components/src/index.js
@@ -7,27 +7,39 @@ import ApprovalCard from './ApprovalCard'
 const App = () => {
     return (
         <div className="ui container comments">
+            <ApprovalCard>
             <CommentDetail
                 author="ashu"
                 timeAgo="today at 9:00AM"
                 content="Nice blog post!"
             />
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail
                 author="spk"
                 timeAgo="today at 10:00AM"
                 content="You can do better"
             />
+            </ApprovalCard>
+            <ApprovalCard>
             <CommentDetail
                 author="abc123"
                 timeAgo="today at 11:00AM"
                 content="I didn't liked this!"
             />
+            </ApprovalCard>
             <ApprovalCard>
                 <CommentDetail
                     author="abc456"
                     timeAgo="today at 11:00AM"
                     content="I didn't liked this!"
                 />
+            </ApprovalCard>
+            <ApprovalCard>
+                <div>
+                    <h4>Warning</h4>
+                    You want to continue?
+                </div>
             </ApprovalCard>
         </div>
     );
