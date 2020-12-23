0d873f7933869eb211780d20f3c3fff595b6719b
spk <ashutoshkarande5@gmail.com>
2020-12-23 13:14:38 +0530

    style

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = function(){
-    return <div>Hi There!</div>;
+    //return <div>Hi There!</div>;
+
+    return <div>
+        <label for="name">Enter Name</label>
+        <input type="text" id="name"/>
+        <button style={{backgroundColor:'red',color:'white'}}>Submit</button>
+    </div>
 };
 
 ReactDOM.render(
