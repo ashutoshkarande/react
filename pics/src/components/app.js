df0f601412ce9bdc10032be63dd3705d75a112b6
spk <ashutoshkarande5@gmail.com>
2020-12-28 15:23:30 +0530

    pics

---
+++ pics/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './searchBar'
+
+const App = () => {
+    return (
+        <div className="ui container" style={{ marginTop: '10px' }}>
+            <SearchBar />
+        </div>
+    );
+}
+
+export default App;
