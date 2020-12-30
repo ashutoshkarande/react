d4fd8a9b9459880eb84af9f8e6bcf5ef9f3c5bba
spk <ashutoshkarande5@gmail.com>
2020-12-30 18:31:36 +0530

    search

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -6,7 +6,9 @@ class App extends React.Component {
 
     render() {
         return (
-            <div ><SearchBar /></div>
+            <div className="ui container">
+                <SearchBar />
+            </div>
         );
     }
 
